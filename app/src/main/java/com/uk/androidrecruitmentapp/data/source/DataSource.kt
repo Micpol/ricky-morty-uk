1f0eefef23c12d337e430445819710b86755dd03
Micpol <michal.polewczak@gmail.com>
2019-11-17 22:01:08 +0100

    Changes call return type, to get info about next page

--- app/src/main/java/com/uk/androidrecruitmentapp/data/source/DataSource.kt
+++ app/src/main/java/com/uk/androidrecruitmentapp/data/source/DataSource.kt
@@ -3,11 +3,12 @@ package com.uk.androidrecruitmentapp.data.source
 import com.uk.androidrecruitmentapp.data.local.Character
 import com.uk.androidrecruitmentapp.data.local.Episode
 import com.uk.androidrecruitmentapp.data.local.Location
+import com.uk.androidrecruitmentapp.data.local.RickyAndMortyResponse
 
 interface DataSource {
     suspend fun loadEpisodes(): Resource<List<Episode>>
 
     suspend fun loadCharacters(): Resource<List<Character>>
 
-    suspend fun loadLocations(): Resource<List<Location>>
+    suspend fun loadLocations(page: Int?): Resource<RickyAndMortyResponse<Location>>
 }
--- app/src/main/java/com/uk/androidrecruitmentapp/data/source/NetworkDataSource.kt
+++ app/src/main/java/com/uk/androidrecruitmentapp/data/source/NetworkDataSource.kt
@@ -3,6 +3,7 @@ package com.uk.androidrecruitmentapp.data.source
 import com.uk.androidrecruitmentapp.data.local.Character
 import com.uk.androidrecruitmentapp.data.local.Episode
 import com.uk.androidrecruitmentapp.data.local.Location
+import com.uk.androidrecruitmentapp.data.local.RickyAndMortyResponse
 import com.uk.androidrecruitmentapp.data.remote.ApiService
 import com.uk.androidrecruitmentapp.data.remote.RequestExecutor
 import com.uk.androidrecruitmentapp.data.remote.response.ApiResponse
@@ -36,10 +37,10 @@ class NetworkDataSource @Inject constructor(
         }.toResource()
     }
 
-    override suspend fun loadLocations(): Resource<List<Location>> {
-        return when (val response = requestExecutor.execute(apiService.loadLocationsAsync())) {
+    override suspend fun loadLocations(page: Int?): Resource<RickyAndMortyResponse<Location>> {
+        return when (val response = requestExecutor.execute(apiService.loadLocationsAsync(page))) {
             is ApiResponse.Success -> {
-                val list = response.data.results
+                val list = response.data
                 ApiResponse.Success(list)
             }
             is ApiResponse.Error -> response
