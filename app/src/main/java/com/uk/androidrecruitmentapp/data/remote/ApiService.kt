3f59e5932982a5ce47f8167e33eeab6bccfd23d0
Micpol <michal.polewczak@gmail.com>
2019-11-17 16:38:46 +0100

    Adds api call to fetch characters

--- app/src/main/java/com/uk/androidrecruitmentapp/data/remote/ApiService.kt
+++ app/src/main/java/com/uk/androidrecruitmentapp/data/remote/ApiService.kt
@@ -1,5 +1,6 @@
 package com.uk.androidrecruitmentapp.data.remote
 
+import com.uk.androidrecruitmentapp.data.local.Characters
 import com.uk.androidrecruitmentapp.data.local.Episodes
 import kotlinx.coroutines.Deferred
 import retrofit2.Response
@@ -9,5 +10,8 @@ interface ApiService {
 
     @GET("episode/")
     fun loadEpisodesAsync(): Deferred<Response<Episodes>>
+
+    @GET("character/")
+    fun loadCharactersAsync(): Deferred<Response<Characters>>
 
 }
