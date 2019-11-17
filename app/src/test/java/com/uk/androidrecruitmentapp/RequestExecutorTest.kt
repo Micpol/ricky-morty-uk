056c06f6c9bb5ed06e20cfc1c7ccaf4a2829f3c8
Micpol <michal.polewczak@gmail.com>
2019-11-17 15:11:38 +0100

    Adds request executor test for failure scenarios

--- app/src/test/java/com/uk/androidrecruitmentapp/RequestExecutorTest.kt
+++ app/src/test/java/com/uk/androidrecruitmentapp/RequestExecutorTest.kt
@@ -9,7 +9,9 @@ import io.mockk.mockk
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.internal.EMPTY_RESPONSE
 import org.junit.Test
+import retrofit2.HttpException
 import retrofit2.Response
 
 @ExperimentalCoroutinesApi
@@ -31,5 +33,36 @@ class RequestExecutorTest {
         val response = requestExecutor.execute(service.loadEpisodesAsync())
 
         assert(response is ApiResponse.Success)
+    }
+
+    @Test
+    fun `error callback is called`() = runBlockingTest {
+        every { service.loadEpisodesAsync() } returns CompletableDeferred(
+                Response.error(401, EMPTY_RESPONSE)
+        )
+
+        val response = requestExecutor.execute(service.loadEpisodesAsync())
+
+        assert(response is ApiResponse.Error)
+    }
+
+    @Test(expected = HttpException::class)
+    fun `connection error`() = runBlockingTest {
+        every { service.loadEpisodesAsync() } throws HttpException(
+                Response.error<Episodes>(401, EMPTY_RESPONSE)
+        )
+
+        val response = requestExecutor.execute(service.loadEpisodesAsync())
+
+        assert(response is ApiResponse.Error)
+    }
+
+    @Test(expected = Throwable::class)
+    fun `response cannot be parsed error`() = runBlockingTest {
+        every { service.loadEpisodesAsync() } throws Throwable()
+
+        val response = requestExecutor.execute(service.loadEpisodesAsync())
+
+        assert(response is ApiResponse.Error)
     }
 }
