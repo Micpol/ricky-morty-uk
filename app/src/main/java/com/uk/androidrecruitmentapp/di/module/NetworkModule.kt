026a602fef4bda607ce75a6b4c441c36be20d7a9
Micpol <michal.polewczak@gmail.com>
2021-02-06 14:24:38 +0100

    Removes redundant modules and old components

--- app/src/main/java/com/uk/androidrecruitmentapp/di/module/NetworkModule.kt
+++ app/src/main/java/com/uk/androidrecruitmentapp/di/module/NetworkModule.kt
@@ -4,6 +4,9 @@ import com.google.gson.Gson
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.uk.androidrecruitmentapp.BuildConfig
 import com.uk.androidrecruitmentapp.data.remote.ApiService
+import com.uk.androidrecruitmentapp.data.source.DataSource
+import com.uk.androidrecruitmentapp.data.source.NetworkDataSource
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +30,15 @@ private const val WRITE_TIMEOUT = 10L
     ]
 )
 abstract class NetworkModule {
+
+    @InstallIn(SingletonComponent::class)
+    @Module
+    abstract class Binders {
+
+        @Binds
+        @Singleton
+        abstract fun bindNetworkDataSource(networkDataSource: NetworkDataSource): DataSource
+    }
 
     @InstallIn(SingletonComponent::class)
     @Module
